f6dec80c2d83104ac133583b4e7af8413c098762
UnTalJohanPerez <kedwin85@hotmail.com>
2016-08-03 13:28:38 -0500

    Layout and Image

--- app/src/main/java/com/developer/ceiba/helloworld/MainActivity.java
+++ app/src/main/java/com/developer/ceiba/helloworld/MainActivity.java
@@ -4,38 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    public int contador;
+    public int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListeners();
+    }
 
+    public void Plus(View v){
+        setCounter(++counter);
+    }
+
+    public void setCounter(int counter){
+        TextView txt = (TextView)findViewById(R.id.numClicks);
+        String txtCount = Integer.toString(counter);
+        if(counter == 1) {
+            txtCount += " Click!";
+        }else{
+            txtCount += " Clicks";
+        }
+        txt.setText(txtCount);
+    }
+
+    public void addListeners(){
         Button btn = (Button) findViewById(R.id.mybtn);
-
+        assert btn != null;
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Plus(v);
             }
         });
-
-        contador = 0;
-        TextView txt = (TextView)findViewById(R.id.texto);
-        String tex = "My contador is: " + Integer.toString(contador);
-        txt.setText(tex);
     }
 
-
-
-    public void Plus(View v){
-        TextView txt = (TextView)findViewById(R.id.texto);
-        contador++;
-        txt.setText("My contador is: " + Integer.toString(contador));
+    public void setImage(){
+        ImageView img = (ImageView) findViewById(R.id.imageViewLogo);
+        img.setImageResource(R.drawable.logo);
     }
 }
